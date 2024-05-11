88e63ec8be203022bdc8e5584336bc9ed680a49c
daveonthegit <114205178+daveonthegit@users.noreply.github.com>
2024-05-11 01:08:39 -0400

    cs fix

--- CountingSort.cpp
+++ CountingSort.cpp
@@ -2,25 +2,29 @@
 #include <unordered_map>
 #include <algorithm>
 #include <chrono>
-
 void countingSort(const std::string& header, std::vector<int> data) {
     auto start = std::chrono::high_resolution_clock::now();
 
     int N = data.size();
-    int data_points = 0; // Track the total number of data points
+    int data_points = 0; // Track the total number of unique data points
     int M = 0; // Finding the maximum element of array inputArray[].
-    std::unordered_map<int, int> countMap; // Hash map to store counts of each data value
+    std::unordered_map<int, bool> uniqueMap; // Hash map to store whether a value is unique
 
+    // Counting unique data points
     for (int i = 0; i < N; i++) {
-        M = std::max(M, data[i]);
-        countMap[data[i]]++; // Update count of data value
+        // Check if the current value has been encountered before
+        if (uniqueMap.find(data[i]) == uniqueMap.end()) {
+            // If not encountered before, increment data_points
+            data_points++;
+            uniqueMap[data[i]] = true; // Mark this value as unique
+        }
+        M = std::max(M, data[i]); // Finding the maximum element
     }
 
-    // Calculating prefix sum at every index of array countArray[]
+    // Sorting the data
     std::vector<int> countArray(M + 1, 0);
-    for (const auto& pair : countMap) {
-        countArray[pair.first] = pair.second;
-        data_points += pair.second; // Increment data points count
+    for (int i = 0; i < N; i++) {
+        countArray[data[i]]++; // Update count of data value
     }
 
     for (int i = 1; i <= M; i++)
@@ -33,18 +37,16 @@ void countingSort(const std::string& header, std::vector<int> data) {
         countArray[data[i]]--;
     }
 
-    data = outputArray;
-
     auto end = std::chrono::high_resolution_clock::now();
     // Calculate the duration in milliseconds
     std::chrono::duration<double> duration = end - start;
 
-    //Output
+    // Output
     std::cout << header << std::endl;
-    std::cout << "Min: " << data[0] << std::endl;
-    std::cout << "P25: " << data[data.size() / 4 - 1] << std::endl;
-    std::cout << "P50: " << data[data.size() / 2] << std::endl;
-    std::cout << "P75: " << data[data.size() / 4 + data.size() / 2 - 1] << std::endl;
-    std::cout << "Max: " << data[data.size() - 1] << std::endl;
+    std::cout << "Min: " << outputArray[0] << std::endl;
+    std::cout << "P25: " << outputArray[N / 4 - 1] << std::endl;
+    std::cout << "P50: " << outputArray[N / 2] << std::endl;
+    std::cout << "P75: " << outputArray[N / 4 + N / 2 - 1] << std::endl;
+    std::cout << "Max: " << outputArray[N - 1] << std::endl;
     std::cout << "Data Points: " << data_points << std::endl;
 }
